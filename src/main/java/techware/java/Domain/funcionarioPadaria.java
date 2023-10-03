b58c279228460715c595ecd91d0e22dce8cafa4f
Mariana Camargo <marianauvmi@gmail.com>
2023-10-02 21:00:23 -0300

    inicializando a criação do sistema da padaria

---
+++ src/main/java/techware/java/Domain/funcionarioPadaria.java
@@ -0,0 +1,12 @@
+package techware.java.Domain;
+
+public class funcionarioPadaria {
+
+    private int id = 0;
+    public String name = "";
+    public String lastName = "";
+    public String phoneNumber = "";
+    public String cpf = "";
+    public String email = "";
+
+}
